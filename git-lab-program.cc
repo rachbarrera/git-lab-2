e8a68b4f46b5904fb3e98f0afb04e65f4d34965e
Rachel Barrera <rb427422@ohio.edu>
2024-09-06 15:18:27 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Rachel Barrera
+ *        Date: 9/6/2024
+ * Description: first C++ program pushed to GitHub
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
